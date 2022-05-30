5d229dd17f77d34c9b9dbb7c5a153a3a56b9fdb5
Hryhorii Tsiupak <Gorg.lvov@Gmail.com>
2022-05-30 23:26:18 +0300

    Reformat code

--- src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
+++ src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
@@ -7,14 +7,12 @@ public class AgeCheckerTest {
     @Test
     public void testAgeUserCanPlay() {
         AgeChecker ageChecker = new AgeChecker();
-        Assert.assertTrue(ageChecker.canPlayGame(19),"Aged user can play game");
-
+        Assert.assertTrue(ageChecker.canPlayGame(19), "Aged user can play game");
     }
+
     @Test
-
-    public  void  testThatTooYongUsersCanNotPlay(){
+    public void testThatTooYongUsersCanNotPlay() {
         AgeChecker ageChecker = new AgeChecker();
-        Assert.assertTrue(ageChecker.testThatTooYongUsersCanNotPlay(16),"You can not play game");
+        Assert.assertTrue(ageChecker.testThatTooYongUsersCanNotPlay(16), "You can not play game");
     }
-
 }
